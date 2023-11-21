b5a68ab73716d668e6dd24c6ff8737d5a7423f32
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-21 11:30:24 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+    this.age = age;
+	this.greet = function () {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    };
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	Person.call(this, name, age);
+    this.jobTitle = jobTitle;
+	this.jobGreet = function () {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    };
+}
 
 // Do not change code below this line
 window.Person = Person;
