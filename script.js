5c982e98127911e8f1214db71badee8ced620a0c
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-21 11:32:57 +0300

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,29 @@
 function Person(name, age) {
 	this.name = name;
     this.age = age;
-	this.greet = function () {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-    };
+}
+
+Person.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 }
 
 function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
     this.jobTitle = jobTitle;
-	this.jobGreet = function () {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-    };
 }
+Employee.prototype = Person.prototype;
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+}
+
+const person = new Person("Alice", 25);
+const employee = new Employee("Alice", 25, "Manager");
+
+person.greet();
+// employee.greet();
+employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
